2454f3ab804aa410f6526d921155654234187406
inseok <inseog1234@gmail.com>
2026-06-29 01:50:16 +0900

    ----

--- Assets/Scripts/Core/SupabaseConfig.cs
+++ Assets/Scripts/Core/SupabaseConfig.cs
@@ -2,8 +2,8 @@ namespace Core
 {
     public static class SupabaseConfig
     {
-        public const string SupabaseUrl = "https://YOUR_PROJECT_ID.supabase.co";
-        public const string AnonPublicKey = "YOUR_SUPABASE_ANON_PUBLIC_KEY";
+        public const string SupabaseUrl = "https://tsvodtpoudczpxfsbktm.supabase.co";
+        public const string AnonPublicKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzdm9kdHBvdWRjenB4ZnNia3RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NTY4NDUsImV4cCI6MjA5ODIzMjg0NX0.LDHdfP4hmPe7fFFeLtQ6QroVeM0WjTQl15qtdmRUqfA";
         public const string CustomLevelTableName = "custom_level_posts";
         public const string CustomLevelLikeTableName = "custom_level_likes";
         public const int ListLimit = 100;
